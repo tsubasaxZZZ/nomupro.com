38aeb5859682a41e9942915247bb1e0ccc37b7c6
tsubasa <t.nomura@qloog.com>
2013-04-09 02:23:30 +0900

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NomuproCom::Application.config.secret_token = 'b3b62ca3fcf83497f8c03159bcbeb1fdb6e444352af581e6dc3add9c0df7064ce62c3bdd84eec38770955e399baf748f27349c40b912f6015fa06349a0b684cb'
